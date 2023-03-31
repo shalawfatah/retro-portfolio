36ac727e0792b0afb4bbb613b356c8899d52f859
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-03-31 00:36:58 -0700

    single page problem

--- src/pages/projects/[id].js
+++ src/pages/projects/[id].js
@@ -5,33 +5,33 @@ import Layout from '../../components/layout/Layout'
 import { graphql } from 'gatsby'
 
 const SingleProject = (params) => {
-  const data = useStaticQuery(graphql`
-    query projectQuery {
-      allSupabaseProject {
-        nodes {
-          id
-          title
-          techstack
-          picture
-          content
-          github
-          live
-        }
-      }
-    }
-  `);
-  const project = data.allSupabaseProject.nodes.find(item => item.id === params.params.id)
-    const githubLink = () => {
-        window.location.href = `${project.github}`;
-    }
-    const liveLink = () => {
-        window.location.href = `${project.live}`;
-    }
+  // const data = useStaticQuery(graphql`
+  //     query projectQuery {
+  //       allSupabaseProject {
+  //         nodes {
+  //           id
+  //           title
+  //           techstack
+  //           picture
+  //           content
+  //           github
+  //           live
+  //         }
+  //       }
+  //     }
+  //   `);
+  // const project = data.allSupabaseProject?.nodes?.find(item => item.id === params.params.id)
+  //   const githubLink = () => {
+  //       window.location.href = `${project.github}`;
+  //   }
+  //   const liveLink = () => {
+  //       window.location.href = `${project.live}`;
+  //   }
 
     return (
     <Layout>
         <div className='flex justify-center items-center flex-col '>
-            <h2 className='text-3xl underline'>{project.title}</h2>
+            {/* <h2 className='text-3xl underline'>{project.title}</h2>
             <div className='flex gap-x-2 my-4'>
             <p className='font-bold'>Tech Stack:</p>
             {project.techstack?.map((item, index) => <p key={index} className='bg-gray-200 rounded px-4 py-1 text-xs'>{item}</p>)}
@@ -44,7 +44,7 @@ const SingleProject = (params) => {
             <div className='flex flex-row gap-x-4'>
             <Button pin={false} text="Code on Github" onClick={githubLink} />
             <Button pin={false} text="Project Live" onClick={liveLink} />
-            </div>
+            </div> */}
         </div>
     </Layout>
   )
