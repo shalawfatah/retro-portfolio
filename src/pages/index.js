19e42f24b8d797544a7b43be7dbd7d1018c94b58
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-03-31 07:21:50 -0700

    layout shift issue resolved

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,15 @@ const IndexPage = () => {
 
   return (
     <Layout classes={"bg-[#CED3DA]"}>
-      <div className="flex lg:justify-around flex-wrap lg:my-32 my-6 p-6 relative container">
-        <div className="mb-60 lg:mb-0 md:mb-0">
-          <Sticky text={"Fourth"} classes={'bg-[#CED3DA] -mt-2 -rotate-[8deg] absolute '} />
-          <Sticky text={"Second"} classes={'bg-[#B1D0FF] -mt-2 -rotate-[6deg] absolute '} />
-          <Sticky text={"Third"} classes={'bg-[#93E496] -mt-2 -rotate-[4deg] absolute '} />
-          <Sticky text={"I'm a web developer, focusing on frontend"} classes={'bg-[#FFEA79] absolute'} />
+      <div className="flex flex-wrap justify-around relative container lg:my-48">
+        <div className=" lg:relative md:relative">
+          <Sticky text={"Fourth"} classes={'bg-[#CED3DA] -mt-2  -rotate-[12deg] absolute hidden lg:block'} />
+          <Sticky text={"Second"} classes={'bg-[#B1D0FF] -mt-2  -rotate-[8deg] absolute hidden lg:block'} />
+          <Sticky text={"Third"} classes={'bg-[#93E496] -mt-2  -rotate-[4deg] absolute hidden lg:block'} />
+          <Sticky text={"I'm a web developer, focusing on frontend"} classes={'bg-[#FFEA79] lg:block lg:absolute z-96'} />
         </div>
-        <div className="lg:block md:block xl:block hidden">
-        <StaticImage src="../images/tea-leaf.png" alt="Tea" className="w-[450px]" />
+        <div className="">
+        <StaticImage src="../images/tea-leaf.png" alt="Tea" className="lg:w-[550px] w-[350px]" />
         </div>
       </div>
       <div className="my-12">
