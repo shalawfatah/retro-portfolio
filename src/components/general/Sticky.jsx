ba01bcd82bedd81f293f1a0330f58dbc8866aff7
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-03-31 23:07:39 -0700

    newer

--- src/components/general/Sticky.jsx
+++ src/components/general/Sticky.jsx
@@ -6,7 +6,7 @@ const Sticky = ({classes, text, image}) => {
     <motion.div drag className={`${classes} 
                 the-girl-next-door cursor-grabbing
                 h-72 w-56 shadow-md shadow-gray-600 
-                flex justify-center items-center
+                flex justify-center items-center flex-col
                 text-center text-2xl
                 py-20 px-4
                 text-[#00366f]
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,14 @@ const IndexPage = () => {
   return (
     <Layout classes={"bg-[#CED3DA]"}>
       <div className="flex flex-wrap justify-around relative container lg:my-48">
-        <div className=" lg:relative md:relative">
-          <Sticky text={"Well, I love tea"} image={<img className="mx-auto my-auto h-20" src={tea} alt="tea" />} classes={'bg-[#CED3DA] -mt-2  -rotate-[12deg] absolute hidden lg:block'} />
-          <Sticky text={"I have a great vision and greater implementation"} classes={'bg-[#B1D0FF] -mt-2  -rotate-[8deg] absolute hidden lg:block'} />
-          <Sticky text={"I use Javascript and React for almost everything"} classes={'bg-[#93E496] -mt-2  -rotate-[4deg] absolute hidden lg:block'} />
-          <Sticky text={"I'm a web developer, focusing on frontend"} classes={'bg-[#FFEA79] lg:block lg:absolute z-96'} />
+        <div className="relative">
+          <Sticky text={"Well, I love tea"} image={<img className="mx-auto my-auto h-20" src={tea} alt="tea" />} classes={'bg-[#CED3DA] -mt-2  -rotate-[12deg] absolute z-48'} />
+          <Sticky text={"I have a great vision and greater implementation"} classes={'bg-[#B1D0FF] -mt-2  -rotate-[8deg] absolute  z-60'} />
+          <Sticky text={"I use Javascript and React for almost everything"} classes={'bg-[#93E496] -mt-2  -rotate-[4deg] absolute z-64'} />
+          <Sticky text={"I'm a web developer, focusing on frontend"} classes={'bg-[#FFEA79] lg:absolute z-96'} />
         </div>
-        <div className="">
-          <Steam />
+        <div className="lg:-mt-40">
+        <Steam />
         <StaticImage src="../images/tea-leaf.png" alt="Tea" className="lg:w-[550px] w-[350px]" />
         </div>
       </div>
