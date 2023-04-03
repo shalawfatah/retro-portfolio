82b43889fa8184abbe4febfadbbfffc871071e09
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-03 04:58:26 -0700

    pointer events removed on images

--- src/components/general/Steam.jsx
+++ src/components/general/Steam.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // STEAM COMPONENT THAT LOOKS LIKE A VAPOR OVER THE TEA CUP ON HOME PAGE
 const Steam = () => {
   return (
-    <div className="vapour">
+    <div className="vapour pointer-events-none">
         <span style={{"--v" :"1"}}></span>
         <span style={{"--v":"2"}}></span>
         <span style={{"--v":"5"}}></span>
