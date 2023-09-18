9348015925eed11a66e7449743b8643b6f783968
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-09-18 13:34:19 -0700

    changes

--- src/components/layout/Button.jsx
+++ src/components/layout/Button.jsx
@@ -6,11 +6,7 @@ import Pin from './Pin';
 const Button = ({text, onClick, pin}) => {
   return (
     <div onClick={onClick} className='relative flex cursor-pointer'>
-        {pin === true ? (<div className='absolute right-0 -top-6 z-[2000]'>
-            <Pin />
-        </div>) : <p></p>}
-        <div className='bg-[#000] m-2 text-center px-10 py-2 ml-[4px] mt-[4px]'>{text}</div>
-        <div className='bg-[#FFC700] m-2 text-center px-8 py-1 absolute'>{text}</div>
+        <div className={`${pin === true ? 'underline' : null} m-2 text-center px-10 py-2 ml-[4px] mt-[4px]`}>{text}</div>
     </div>
   )
 }
